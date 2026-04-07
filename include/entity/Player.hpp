d327b72d361fd65ce2c5a5187c42747564851ce1
RFihobiana <RaFihobiananyAvo@gmail.com>
2026-04-07 06:12:34 +0300

    Generalize keyboard handling on player

--- include/entity/Player.hpp
+++ include/entity/Player.hpp
@@ -1,11 +1,34 @@
 #pragma once
 
+#include "command/Command.hpp"
 #include "command/CommandQueue.hpp"
 #include <SFML/Window/Event.hpp>
+#include <SFML/Window/Keyboard.hpp>
+#include <unordered_map>
 class Player {
     /* This class give an order to player's aircraft */
+    public:
+        enum Action {
+            MoveLeft,
+            MoveRight,
+            MoveUp,
+            MoveDown,
+
+            ShowPosition,
+        };
+
     public:
         Player();
         void handle_event(const sf::Event& event, CommandQueue& commands);
         void handle_realtime_event(CommandQueue& commands);
+
+        void assign_key(const sf::Keyboard::Key& key, const Action& action);
+        sf::Keyboard::Key get_assigned_key(const Action& action) const;
+
+    private:
+        static bool is_realtime_action(const Action& action);
+    
+    private:
+        std::unordered_map<sf::Keyboard::Key, Action>   m_key_binding;
+        std::unordered_map<Action, Command>             m_action_binding;
 };
--- src/entity/Player.cpp
+++ src/entity/Player.cpp
@@ -8,7 +8,9 @@
 #include <SFML/System/Vector2.hpp>
 #include <SFML/Window/Event.hpp>
 #include <SFML/Window/Keyboard.hpp>
+#include <algorithm>
 #include <iostream>
+#include <utility>
 
 struct AircraftMover {
     AircraftMover(const float vx, const float vy): velocity(vx, vy) {}
@@ -21,61 +23,69 @@ struct AircraftMover {
 };
 
 Player::Player() {
+    const float player_speed = 200.f;
+
+    m_key_binding[sf::Keyboard::W] = MoveUp;
+    m_key_binding[sf::Keyboard::S] = MoveDown;
+    m_key_binding[sf::Keyboard::A] = MoveLeft;
+    m_key_binding[sf::Keyboard::D] = MoveRight;
+    m_key_binding[sf::Keyboard::P] = ShowPosition;
+
+    m_action_binding[MoveLeft].action = derivated<Aircraft>(AircraftMover(-player_speed, 0.f));
+    m_action_binding[MoveRight].action = derivated<Aircraft>(AircraftMover(player_speed, 0.f));
+    m_action_binding[MoveUp].action = derivated<Aircraft>(AircraftMover(0.f, -player_speed));
+    m_action_binding[MoveDown].action = derivated<Aircraft>(AircraftMover(0.f, player_speed));
+    m_action_binding[ShowPosition].action = derivated<Aircraft>([](Aircraft& aircraft, sf::Time&) {
+        const sf::Vector2f pos = aircraft.getPosition();
+        std::cout
+            << "P: ("
+            << pos.x << ", " << pos.y
+            << ")"
+            << std::endl;
+    });
+
+    for(auto& pair: m_action_binding) pair.second.category = Category::PlayerAircraft;
 }
 
 void Player::handle_event(const sf::Event& event, CommandQueue& commands) {
     if(event.type == sf::Event::KeyReleased) {
-        if(event.key.code == sf::Keyboard::P) {
-            Command show_position {
-                .action = derivated<Aircraft>([](Aircraft& target, sf::Time& dt) {
-                    const auto pos = target.getPosition();
-                    std::cout
-                        << "Player position: ("
-                        << pos.x << ", " << pos.y
-                        << ")"
-                        << std::endl;
-
-                }),
-                .category = Category::PlayerAircraft
-            };
-            commands.push_back(show_position);
+        for(const auto& pair: m_key_binding) {
+            if(!is_realtime_action(pair.second) && event.key.code == pair.first) {
+                commands.push_back(m_action_binding[pair.second]);
+            }
         }
     }
 }
+
 void Player::handle_realtime_event(CommandQueue& commands) {
-    const float speed = 200.f;
-
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-        Command move_up {
-            .action     = derivated<Aircraft>(AircraftMover(0.f, -speed)),
-            .category   = Category::PlayerAircraft
-        };
-
-        commands.push_back(move_up);
+    for(const auto& pair: m_key_binding) {
+        if(is_realtime_action(pair.second) && sf::Keyboard::isKeyPressed(pair.first)) {
+            commands.push_back(m_action_binding[pair.second]);
+        }
     }
+}
 
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-        Command move_down {
-            .action     = derivated<Aircraft>(AircraftMover(0.f, speed)),
-            .category   = Category::PlayerAircraft
-        };
-        commands.push_back(move_down);
+void Player::assign_key(const sf::Keyboard::Key& key, const Action& action) {
+    m_key_binding[key] = action;
+}
+
+sf::Keyboard::Key Player::get_assigned_key(const Action& action) const {
+    auto iter = std::find_if(m_key_binding.begin(), m_key_binding.end(), [&action](std::pair<sf::Keyboard::Key, Action> pair) {
+        return pair.second == action;
+    });
+
+    if(iter == m_key_binding.end()) return sf::Keyboard::Unknown;
+    return iter->first;
+}
+
+bool Player::is_realtime_action(const Action& action) {
+    switch (action) {
+        case MoveLeft:
+        case MoveRight:
+        case MoveUp:
+        case MoveDown:
+            return true;
+        
+        default: return false;
     }
-
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-        Command move_left {
-            .action = derivated<Aircraft>(AircraftMover(-speed, 0.f)),
-            .category = Category::PlayerAircraft
-        };
-        commands.push_back(move_left);
-    }
-
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-        Command move_right {
-            .action     = derivated<Aircraft>(AircraftMover(speed, 0.f)),
-            .category   = Category::PlayerAircraft
-        };
-        commands.push_back(move_right);
-    }
-
 }
